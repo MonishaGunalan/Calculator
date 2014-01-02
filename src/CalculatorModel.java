0a97d56ab55f75bc88444b6d647694d35f078caf
Monisha Gunalan <moni.sind@gmail.com>
2014-01-02 16:25:42 -0500

    added code to perform calculation and to notify observers

--- src/CalculatorModel.java
+++ src/CalculatorModel.java
@@ -1,9 +1,45 @@
 import java.util.Observable;
 
-
 public class CalculatorModel extends Observable {
+	private int operator1, operator2, result;
+	private char operand;
 
-	public CalculatorModel(){
-		
+	public CalculatorModel(CalculatorController controller) {
+		CalculatorView view = new CalculatorView(controller);	
+		this.addObserver(view);
 	}
+	
+	public void setOperator1(String operator1){
+		this.operator1 = Integer.parseInt(operator1);
+	}
+	
+	public void setOperator2(String operator2){
+		this.operator2 = Integer.parseInt(operator2);
+	}
+	
+	public void setOperand(String operand){
+		this.operand = operand.charAt(0);
+	}
+	
+	public void performCalculation(){
+		switch(operand){
+		case '+': result = operator1 + operator2;
+				break;
+		case '-': result = operator1 - operator2;
+				break;
+		case '*': result = operator1 * operator2;
+				break;
+		case '/': result = operator1 / operator2;
+				break;
+		}
+		
+		System.out.println(" "+ result);
+		display("" + result);
+	}
+	
+	public void display(String value){
+		this.setChanged();
+		this.notifyObservers(value);
+	}
+	
 }
