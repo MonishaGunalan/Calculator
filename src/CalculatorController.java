b4b2e64ecb4d9b6cb6351120a86d1914edc4dd69
Monisha Gunalan <moni.sind@gmail.com>
2014-01-02 13:25:48 -0500

    created and implemented the actionPerformed method in controller for the button press

--- src/CalculatorController.java
+++ src/CalculatorController.java
@@ -3,6 +3,15 @@ import java.awt.event.ActionListener;
 
 public class CalculatorController implements ActionListener {
 
+	private String operator1 = "";
+	private String operator2 = "";
+	private String operand = "";
+	private String equals = "";
+	private boolean receivedOp1 = false;
+	private boolean receivedOp2 = false;
+	private boolean receivedOperand = false;
+	private boolean receivedEquals = false;
+
 	public CalculatorController() {
 		CalculatorModel model = new CalculatorModel();
 		CalculatorView view = new CalculatorView(this);
@@ -11,25 +20,34 @@ public class CalculatorController implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String buttonPressed = e.getActionCommand();
-		String operator1 = "";
-		String operator2 = "";
-		String operand = "";
-		String equals = "";
-		boolean receivedOp1 = false;
-		boolean receivedOp2 = false;
-		boolean receivedOperand = false;
-		
-		
-		if(isInt(buttonPressed) && !receivedOp1){
+		// System.out.println(buttonPressed);
+
+		if (isInt(buttonPressed) && !receivedOp1) {
 			operator1 += Integer.parseInt(buttonPressed);
-		}else if (!isInt(buttonPressed)){
+			System.out.println("Operator1: " + operator1);
+
+		} else if (validOperand(buttonPressed)) {
 			receivedOp1 = true;
 			operand = buttonPressed;
 			receivedOperand = true;
-		}else if(isInt(buttonPressed) && receivedOp1 && !receivedOp1 ){
+			System.out.println("Operand: " + operand);
+
+		} else if (isInt(buttonPressed) && receivedOp1 && !receivedOp2) {
 			operator2 += Integer.parseInt(buttonPressed);
-		}else if(!isInt(buttonPressed) && buttonPressed.equals("=")){
-			equals = 
+			System.out.println("Operator2: " + operator2);
+			
+		} else if (buttonPressed.equals("=")) {
+			receivedOp2 = true;
+			equals = buttonPressed;
+			receivedEquals = true;
+			System.out.println("Equals: " + equals);
+
+		} else if (buttonPressed.equals("C")) {
+			System.out.println("Reset. Ready for next calculation");
+			reset();
+		
+		} else {
+			System.out.println("Invalid Input");
 		}
 	}
 
@@ -40,6 +58,25 @@ public class CalculatorController implements ActionListener {
 		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	public boolean validOperand(String buttonPressed) {
+		if (buttonPressed.equals("+") || buttonPressed.equals("-")
+				|| buttonPressed.equals("*") || buttonPressed.equals("/")) {
+			return true;
+		}
+		return false;
+	}
+
+	public void reset() {
+		operator1 = "";
+		operator2 = "";
+		operand = "";
+		equals = "";
+		receivedOp1 = false;
+		receivedOp2 = false;
+		receivedOperand = false;
+		receivedEquals = false;
 	}
 
 	public static void main(String[] args) {
